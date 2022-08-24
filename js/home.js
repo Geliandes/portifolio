374f35f02069865f8732137a4624036569e4b9e1
Paulo Geliandes <geliandes@hotmail.com>
2022-08-23 21:01:20 -0300

    update

--- js/home.js
+++ js/home.js
@@ -1,14 +1,3 @@
-function sectionSelect(selection) {
-
-	var selectors = document.querySelectorAll(".line");
-
-	for (let i = 0; i < selectors.length; i++) {
-		selectors[i].classList.remove('selected')
-	}
-
-	selectors[selection].classList.add('selected')
-}
-
 $(function() {
 	var area1 = $('#firstSelector').offset().top - $(window).height();
 	var area2 = $('#secondSelector').offset().top - $(window).height();
@@ -18,7 +7,7 @@ $(function() {
 	var selectors = document.querySelectorAll(".line");
 
 	$(window).scroll(function() {
-		if ($(window).scrollTop() > area1) {
+		if ($(window).scrollTop() >= area1) {
 
 			for (let i = 0; i < selectors.length; i++) {
 				selectors[i].classList.remove('selected')
@@ -50,6 +39,5 @@ $(function() {
 			}
 			selectors[3].classList.add('selected')
 		}
-
 	});
 });
