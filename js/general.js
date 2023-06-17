04446dc847d8bd6c7176bfa4a2fb3ce50254d80f
Paulo Geliandes <geliandes@hotmail.com>
2023-06-17 16:51:58 -0300

    Update general.js

--- js/general.js
+++ js/general.js
@@ -1,20 +1,44 @@
-function btnMenu(){
-    let button = document.querySelector("#btnMenu");
-    let menuMobile = document.querySelector("#menuMobileContainer")
+function btnMenu() {
+  let button = document.querySelector('#btnMenu');
+  let menuMobile = document.querySelector('#menuMobileContainer');
 
-    if(button.classList == 'btnActive'){ 
-        button.classList.remove('btnActive');
-        button.classList.add("opened");
-        menuMobile.style.width="0%";
-    } else{
-        button.classList.add('btnActive');
-        button.classList.remove("opened");
-        menuMobile.style.width="100%";
-    }
+  if (button.classList == 'btnActive') {
+    button.classList.remove('btnActive');
+    button.classList.add('opened');
+    menuMobile.style.width = '0%';
+  } else {
+    button.classList.add('btnActive');
+    button.classList.remove('opened');
+    menuMobile.style.width = '100%';
+  }
 }
 
-function closeMenu(){
-    closeMenuOption = document.querySelector("#btnMenu");
+function closeMenu() {
+  closeMenuOption = document.querySelector('#btnMenu');
 
-    closeMenuOption.click();
+  closeMenuOption.click();
 }
+
+function performHeavyTask() {
+  // Simula um processamento intensivo
+  let result = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    result += i;
+  }
+  return result;
+}
+
+function executeHeavyScript() {
+  console.log('Início do script pesado.');
+  const startTime = performance.now();
+
+  // Executa a tarefa pesada várias vezes para aumentar a carga
+  for (let i = 0; i < 10; i++) {
+    performHeavyTask();
+  }
+
+  const endTime = performance.now();
+  console.log(`Fim do script pesado. Tempo total: ${endTime - startTime}ms.`);
+}
+
+executeHeavyScript();
